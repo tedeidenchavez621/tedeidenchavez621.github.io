29a8e60b65ca1e93122177a36704fe6bb4f57766
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-06 13:03:01 +0800

    2/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import duduBubu from './assets/dudu-bubu.gif';
 
@@ -5,9 +6,8 @@ const App = () => {
     return (
         <div className="container">
             <img src={String(duduBubu)} alt="Bubu and Dudu" />
-            <form>
-
-            </form>
+            <h2>Hello </h2>
+            <button>CONTINUE</button>
         </div>
     );
 };
