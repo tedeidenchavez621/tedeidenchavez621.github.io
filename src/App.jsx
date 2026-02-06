c84c7a2272036ac7e8d5f3305a48fa7dfc5a9cb5
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-06 16:44:52 +0800

    views

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
 import './App.css';
 import duduBubu from './assets/dudu-bubu.gif';
+import catDancing from './assets/cat-dancing.gif';
 
 const App = () => {
+    const [isSwapped, setIsSwapped] = useState(false);
+
     return (
         <div className="container">
-            <img src={String(duduBubu)} alt="Bubu and Dudu" />
-            <h2>Hello </h2>
-            <button>CONTINUE</button>
+            <div className="content-area">
+                <h2>Hello</h2>
+                {isSwapped ? (
+                    <>
+                        <h1>This is page 1</h1>
+                        <img src={duduBubu} alt="Bubu and Dudu" />
+                    </>
+                ) : (
+                    <>
+                        <h1>This is page 2</h1>
+                        <img src={catDancing} alt="Cat Dancing" />
+                    </>
+                )}
+            </div>
+            <button onClick={() => setIsSwapped(!isSwapped)}>CONTINUE</button>
         </div>
     );
 };
